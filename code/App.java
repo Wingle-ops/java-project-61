eac35245905928b766ddbaa066366b453f232310
Mihail <tchechin.mihael@gmail.com>
2024-11-28 19:29:04 +0300

    обновление файлов

--- code/App.java
+++ code/App.java
@@ -1,12 +1,11 @@
 package hexlet.code;
 
+import java.util.Scanner;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
 import hexlet.code.games.Prime;
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
---
+++ code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static String nameUser;
+
+    static void name() {
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        nameUser = name;
+        System.out.println("Hello, " + name + "!");
+    }
+}
